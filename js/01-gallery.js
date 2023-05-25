0a1625d3fc4f1d62e020b73a306df53bf15df359
Svitsa Mariia <mariia0198@gmail.com>
2023-05-25 15:51:17 +0300

    fixed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,11 @@
-import {galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
 
 const gallery = document.querySelector(".gallery");
 const galleryMarkup = galleryItems
     .map(({original, preview, description}) =>
-        `<li class = "gallery__item">
-<a class = "gallery__link" href = ${original}><img src = ${preview} alt = ${description}
+        `<li class = "gallery__item"><a class = "gallery__link" href = ${original}><img src = ${preview} alt = ${description}
 data-source = ${original} class = "gallery__image"></a></li>`)
     .join(""); 
 
