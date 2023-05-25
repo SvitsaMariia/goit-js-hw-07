0116241354e17c923df34f8b847778587d9d6ee1
Svitsa Mariia <mariia0198@gmail.com>
2023-05-25 13:59:13 +0300

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+    .map(({original, preview, description }) =>
+        `<li class = "gallery__item">
+<class = "gallery__link" href = ${preview}> <img src = ${original} alt = ${description}
+data-source = ${original} class = "gallery__image"></li>`)
+    .join(""); 
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+const onImageClick = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src = ${event.target.dataset.source} alt = ${event.target.alt}
+        width = "800" height = "600" >`,
+     {
+     onClose: (instance) => gallery.removeEventListener("keydown", closeByEscape),
+        }
+    );
+     const closeByEscape = (key) => {
+        if (key.code === "Escape") {
+            instance.close();
+        }
+    };
+    instance.show();
+    if (instance.visible()) {
+        gallery.addEventListener("keydown", closeByEscape);
+    }
+};
+gallery.addEventListener("click", onImageClick);
+
+// console.log(galleryItems);
